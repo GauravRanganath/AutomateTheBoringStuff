534ac001aecdb7024a8cab26e750e5a65802499c
GauravRanganath <46269824+GauravRanganath@users.noreply.github.com>
2019-01-01 16:11:09 -0500

    Create helloWorld.py

---
+++ CH01/helloWorld.py
@@ -0,0 +1,15 @@
+# This program says hello and aks for my name.
+
+print('Hello World!')
+print('What is your name?')
+
+myName = input()
+
+print('Nice to meet you ' + myName)
+print('The length of your name is ')
+print(len(myName))
+print('What is your age?')
+
+myAge = input()
+
+print('Your age is ' + myAge)
